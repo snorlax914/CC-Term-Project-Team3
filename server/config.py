867cbe2db0b314ec65796a7242879db5203d50e2
KenesYerassyl <kenesyerassyl@gmail.com>
2025-05-17 22:05:19 +0900

    config file

---
+++ server/config.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    GITHUB_CLIENT_ID = os.getenv('GITHUB_CLIENT_ID')
+    GITHUB_CLIENT_SECRET = os.getenv('GITHUB_CLIENT_SECRET')
+    GITHUB_AUTHORIZE_URL = 'https://github.com/login/oauth/authorize'
+    GITHUB_TOKEN_URL = 'https://github.com/login/oauth/access_token'
+    
+    SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
+    CALLBACK_URL = 'http://localhost:5001/api/auth/callback/github'
+    
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') + '?sslmode=require'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS ={
+        'pool_pre_ping': True,
+        'pool_recycle': 300,
+        'pool_size': 10,
+        'max_overflow': 20,
+    }
